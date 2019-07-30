154982f8f75296373afd3d5723f4e5b6c6f977b0
Saad Albasha <SaadA@leading-point.com>
2019-07-30 11:38:55 +0300

    preparing to cocoapods1

---
+++ AASnackbar/AASnackbar/AASnackbar.h
@@ -0,0 +1,19 @@
+//
+//  AASnackbar.h
+//  AASnackbar
+//
+//  Created by Mobile build server on 7/30/19.
+//  Copyright © 2019 Saad Albasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AASnackbar.
+FOUNDATION_EXPORT double AASnackbarVersionNumber;
+
+//! Project version string for AASnackbar.
+FOUNDATION_EXPORT const unsigned char AASnackbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AASnackbar/PublicHeader.h>
+
+
